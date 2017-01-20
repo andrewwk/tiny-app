7e3f975283c2dd8d25fb200fb75f3702f14592d4
Andrew Kim <your@email.com>
2017-01-20 16:31:54 -0500

    Went over stack requirements. Adjusted naming of routes. Added in redirects, logic for unauthrized users etc.

--- express_server.js
+++ express_server.js
@@ -1,11 +1,10 @@
 const express       = require('express')
 const cookieSession = require('cookie-session')
 const session       = require('express-session')
-// const cookieParser  = require('cookie-parser')
 const flash         = require('connect-flash')
-const app           = express()
 const bodyParser    = require('body-parser')
 const bcrypt        = require('bcrypt');
+const app           = express()
 const PORT          = process.env.PORT || 8080 // default port 8080
 
 const generateRandomString = () => {
@@ -46,14 +45,33 @@ const checkEmail = (email) => {
   return false;
 }
 
+const verifyURLOwner = (url, user_id) => {
+  let verify = false;
+  for (key in users) {
+    if (users[user_id].urls[url]) {
+      verify = true
+    }
+  }
+  return verify;
+}
+
+const verifyShortURL = (shortURL) => {
+  let verify = false;
+  for (key in users) {
+    if (users[key].urls[shortURL]) {
+      verify = true
+    }
+  }
+  return verify;
+}
+
 app.set('view engine', 'ejs')
 
 app.use(cookieSession({
   name: 'session',
   keys: ['supasecret_secret'],
-  maxAge: 24 * 60 * 60 * 1000 // 24 hours
+  maxAge: 60 * 60 * 1000 // 1 hour
 }))
-// app.use(cookieParser('supasecret_secret'))
 app.use(session({
   cookie: { maxAge: 60000 },
   secret: 'uber_supasecret_secret',
@@ -64,15 +82,30 @@ app.use(flash())
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get('/', (req, res) => res.redirect('/login'))
+app.get('/', (req, res) => {
+  if (req.session.user_id) {
+    res.redirect('/login')
+  } else {
+    res.redirect('/register')
+  }
+})
+
+app.get('/error', (req, res) => {
+  const locals = {
+    users: users,
+    message: req.flash('info'),
+    user_id: req.session.user_id
+  }
+  res.render('error', locals)
+})
 
 app.get('/login',(req, res) => {
   if (req.session.user_id) {
     res.redirect('/urls')
   } else {
-    res.render('login')
+    res.status(200).render('login')
   }
-});
+})
 
 app.post('/login', (req, res) => {
   let email = req.body.email
@@ -82,9 +115,10 @@ app.post('/login', (req, res) => {
   if (user && (bcrypt.compareSync(password, user.password) || user.password === password)) {
     user_id = user.id
     req.session.user_id = user_id
-    res.redirect('/urls')
+    res.redirect('/')
   } else {
-    res.sendStatus(403)
+    req.flash('info', '401: Unauthorized User. Please login.')
+    res.status(401).redirect('/error')
   }
 });
 
@@ -107,14 +141,22 @@ app.post('/register', (req, res) => {
     }
     req.session.user_id = user_id
     res.redirect('/')
+  } else if (email.length === 0 || password.length === 0) {
+    req.flash('info', '400: Please enter passsword AND email.')
+    res.status(400).redirect('/error')
   } else {
-    res.sendStatus(403)
+    req.flash('info', '400: Email is already registered')
+    res.status(400).redirect('/error')
   }
 })
 
 app.get('/register', (req, res) => {
   let locals = { user_id: req.session.user_id }
-  res.render('register', locals)
+  if (req.session.user_id) {
+    res.redirect('/')
+  } else {
+    res.status(200)render('register', locals)
+  }
 })
 
 app.get('/urls', (req, res) => {
@@ -123,56 +165,102 @@ app.get('/urls', (req, res) => {
     message: req.flash('info'),
     user_id: req.session.user_id
   };
-  res.render('urls_index', locals)
+  if (!req.session.user_id) {
+    req.flash('info', '401: Unauthorized User. Please login.')
+    res.status(401).redirect('/error')
+  } else {
+    res.status(201).render('urls_index', locals)
+  }
+})
+
+app.post('/urls', (req, res) => {
+  let longURL  = req.body.longURL
+  let shortURL = generateRandomString()
+  let user_id  = req.session.user_id
+  if(!user_id) {
+    req.flash('info', '401: Unauthorized User. Please login.')
+    res.status(401).redirect('/error')
+  } else {
+    users[user_id].urls[shortURL] = longURL
+    res.redirect(`/urls/${shortURL}`)
+  }
 })
 
 app.get('/urls/new', (req, res) => {
   const locals = {
     user_id: req.session.user_id,
+    message: req.flash('info'),
     users: users
    }
-  res.render('urls_new', locals)
-});
-
-app.post('/urls/create', (req, res) => {
-  let longURL = req.body.longURL
-  let shortURL = generateRandomString()
-  let user_id = req.session.user_id
-  users[user_id].urls[shortURL] = longURL
-  res.redirect('/urls')
+   if (!req.session.user_id) {
+     req.flash('info', '401: Unauthorized User. Please login.')
+     res.status(401).redirect('/error')
+   } else {
+     res.status(200).render('urls_new', locals)
+   }
 })
 
 app.get('/u/:shortURL', (req, res) => {
   let user_id = req.session.user_id
-  let urls = users[user_id].urls
+  let urls    = users[user_id].urls
   let longURL = urls[req.params.shortURL]
-  res.redirect(longURL)
+  if (!longURL) {
+    req.flash('info', '404: URL not found.')
+    res.status(404).redirect('/error')
+  } else {
+    res.redirect(longURL)
+  }
 })
 
 app.post('/urls/:id/delete', (req, res) => {
   let user_id = req.session.user_id
-  let urls = users[user_id].urls
-  delete urls[req.params.id]
-  req.flash('info', 'Url Delete Successful!')
-  res.redirect('/urls')
+  let urls    = users[user_id].urls
+  if (!verifyURLOwner(req.params.id, user_id)) {
+    req.flash('info', '403: FORBIDDEN! Unauthorized user. You are not the url owner.')
+    res.status(403).redirect('/error')
+  } else {
+    delete urls[req.params.id]
+    req.flash('info', 'Url Delete Successful!')
+    res.redirect('/urls')
+  }
 })
 
 app.get('/urls/:id', (req, res) => {
   let locals = {
     users: users,
     shortURL: req.params.id,
-    user_id: req.session.user_id
+    user_id: req.session.user_id,
+    message: req.flash('info')
   }
-  res.render('urls_show', locals)
+  if (!verifyShortURL(req.params.id)) {
+    req.flash('info', '404: Short URL not found.')
+    res.status(404).redirect('/error')
+  } else if (!req.session.user_id) {
+    req.flash('info', '401: Unauthorized user. Please login.')
+    res.status(401).redirect('/error')
+  } else if (!verifyURLOwner(req.params.id, req.session.user_id)) {
+    req.flash('info', '403: Forbidden! This url is not registered to you.')
+    res.status(403).redirect('/error')
+  } else {
+    res.status(200).render('urls_show', locals)
+  }
 })
 
 app.post('/urls/:id', (req, res) => {
   let user_id = req.session.user_id
   let urls = users[user_id].urls
-  for (url in urls) {
-    urls[req.params.id] = req.body.updated_url
+  if (req.body.updated_url.length < 1) {
+    req.flash('info', 'It appears you forgot to enter a new url.')
+    res.redirect('/urls')
+  } else if (!user_id) {
+    req.flash('info', '401: Unauthorized user. Please login.')
+    res.status(401).redirect('/error')
+  } else {
+    for (url in urls) {
+      urls[req.params.id] = req.body.updated_url
+    }
+    res.redirect(`/urls/${req.params.id}`)
   }
-  res.redirect('/urls')
 })
 
 app.use((req, res) => res.status(404).send('Error 404. This path does not exist.'))
